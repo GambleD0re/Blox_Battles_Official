cbb824968a6f2e027d73c580f4783c08691b8c6f
GambleD0re <gambledorethewealthy@gmail.com>
2025-08-13 12:22:11 +0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -126,19 +126,19 @@ style.innerHTML = `
         top: 50%;
         transform: translateY(-50%);
         transition: left 0.8s cubic-bezier(0.2, 0.8, 0.2, 1);
-        width: 48rem;
+        width: 36rem;
     }
     .pos-slot2 {
         left: 1rem;
     }
     .pos-slot1 {
-        left: calc(100% - 48rem - 1rem);
+        left: calc(100% - 36rem - 1rem);
     }
     .pos-enter {
         left: 100%;
     }
     .pos-exit {
-        left: -48rem;
+        left: -36rem;
     }
 `;
 document.head.appendChild(style);
